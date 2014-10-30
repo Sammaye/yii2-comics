921b132af6cc5a5b95fbcc50184088d792bbab66
Sammaye <sam.millman@googlemail.com>
2014-10-30 13:02:39 +0000

    Cleaned up the console command

--- console/controllers/ScraperController.php
+++ console/controllers/ScraperController.php
@@ -5,6 +5,7 @@ namespace console\controllers;
 use Yii;
 use yii\console\Controller;
 use common\models\ComicStrip;
+use common\models\Comic;
 
 class ScraperController extends Controller
 {
@@ -13,15 +14,20 @@ class ScraperController extends Controller
 		return '[ '.date('d-m-Y H:i:s').' '.microtime(true).' ] '.$message."\n";
 	}
 	
-	public function actionCheckTimeOfNew()
+	public function actionCheckTimeOfNew($comic_id)
 	{
+		if(!($comic = Comic::find()->where(['id' => new \MongoId($comic_id)])->one())){
+			echo $this->log('That comic does not exist! Try another.');
+			return 1;
+		}
+		
 		while(true){
 			
 			$date = new \DateTime();
 			$date->setDate(2014, 10, 18);
 			
 			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, 'http://garfield.com/comic/' . $date->format('Y-m-d'));
+			curl_setopt($ch, CURLOPT_URL, $comic->scrape_url . $date->format($comic->date_format));
 			curl_setopt($ch, CURLOPT_HEADER, true);
 			curl_setopt($ch, CURLOPT_NOBODY, true); // remove body
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -31,7 +37,7 @@ class ScraperController extends Controller
 			
 			if($httpCode == 200){
 				echo $this->log("IT'S HERE IT'S HERE !!!!111");
-				$this->actionGet($date->format('Y'), $date->format('m'), $date->format('d'));
+				$this->actionGetDate($date->format('d'), $date->format('m'), $date->format('Y'), (String)$comic->_id);
 				break;
 			}else{
 				echo $this->log("still not there");
@@ -40,52 +46,55 @@ class ScraperController extends Controller
 		}
 	}
 	
-	public function actionGet($year = 2014, $month = 10, $day = 17)
+	public function actionGetToday($comic_id = null)
+	{
+		$date = new \DateTime();
+		$date->setDate(date('Y'), date('m'), date('d'));
+		$ts = mktime(0, 0, 0, $date->format('m'), $date->format('d'), $date->format('Y'));
+		return $this->get($ts, $comic_id);
+	}
+
+	public function actionGetDate($day, $month, $year, $comic_id = null)
 	{
 		$date = new \DateTime();
 		$date->setDate($year, $month, $day);
 		$ts = mktime(0, 0, 0, $date->format('m'), $date->format('d'), $date->format('Y'));
-		
-		if(ComicStrip::find()->where(['date' => new \MongoDate($ts)])->one()){
-			return;
-		}
-		
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Googlebot/2.1 (http://www.googlebot.com/bot.html)');
-		curl_setopt($ch, CURLOPT_URL, 'http://garfield.com/comic/' . $date->format('Y-m-d'));
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$body = curl_exec($ch);
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_close($ch);
-		
-		$doc = new \DOMDocument();
-		libxml_use_internal_errors(true);
-		$doc->loadHtml($body);
-		libxml_clear_errors();
-		
-		$el = $doc->getElementById('comic_wrap');
-		
-		$url = null;
-		foreach($el->childNodes as $child){
-			if($child instanceof \DOMElement && $child->tagName == 'img'){
-				$url = $child->getAttribute('src');
+		return $this->get($tds, $comic_id);
+	}
+	
+	private function get($ts, $comic_id = null)
+	{
+		if($comic_id){
+			if(ComicStrip::find()->where(['comic_id' => new \MongoId($comic_id), 'date' => new \MongoDate($ts)])->one()){
+				echo $this->log('Strip for ' . date('d/m/Y') . ' : ' . $comic->title . ' already exists');
+				return 1;
+			}
+			
+			$strip = new ComicStrip();
+			$strip->date = new \MongoDate($ts);
+			$strip->populateRemoteImage();
+			if($strip->save()){
+				$this->log('Strip for ' . date('d/m/Y') . ' : ' . $comic->title . ' was saved successfully');
+			}else{
+				$this->log('Strip for ' . date('d/m/Y') . ' : ' . $comic->title . ' did not save');
+			}
+		}else{
+			foreach(Comic::find()->all() as $comic){
+				if(ComicStrip::find()->where(['comic_id' => $comic->_id, 'date' => new \MongoDate($ts)])->one()){
+					// Don't drown us in crappy messages
+					continue;
+				}
+				
+				$strip = new ComicStrip();
+				$strip->date = new \MongoDate($ts);
+				$strip->populateRemoteImage();
+				if($strip->save()){
+					echo $this->log('Strip for ' . date('d/m/Y') . ' : ' . $comic->title . ' was saved successfully');
+				}else{
+					echo $this->log('Strip for ' . date('d/m/Y') . ' : ' . $comic->title . ' did not save');
+				}
 			}
 		}
-		
-		if(!$url){
-			echo $this->log("ERROR! Could not find the image for some reason...");
-		}
-		
-		$strip = new ComicStrip();
-		$strip->date = new \MongoDate($ts);
-		$strip->url = $url;
-		$strip->img = new \MongoBinData(file_get_contents($url));
-		
-		if(!$strip->save()){
-			echo $this->log("ERROR! model could not save because: " . var_export($strip->getErrors(), true));
-		}
-		
-		echo $this->log("SUCCESS! Strip was downloaded");
+		return 0;
 	}
 }
