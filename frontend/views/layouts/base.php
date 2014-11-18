d9f0562ea1b15c1a349afb77ad6aacdb219d785c
Sammaye <sam.millman@googlemail.com>
2014-11-18 23:04:28 +0000

    Added Google Analytics

--- frontend/views/layouts/base.php
+++ frontend/views/layouts/base.php
@@ -34,6 +34,15 @@ AppAsset::register($this);
     <?php $this->beginBody() ?>
     <?= $content ?>
     <?php $this->endBody() ?>
+    
+    <script>
+  	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  	ga('create', 'UA-56876004-1', 'auto');
+  	ga('send', 'pageview');
+	</script>
 </body>
 </html>
 <?php $this->endPage() ?>
