ce256ef5fef1e433b56b41b2620d2f90eaa564ca
Sam <sam.millman@googlemail.coom>
2017-05-06 14:08:46 +0100

    Removed the subdocuments extension and replaced it for the yii2tech one

--- common/components/ActiveRecord.php
+++ common/components/ActiveRecord.php
@@ -3,26 +3,10 @@
 namespace common\components;
 
 use Yii;
-use common\components\Subdocument;
-use yii\mongodb\ActiveRecord as MongoActiveRecord;
-use common\components\ActiveQuery;
+use yii\mongodb\ActiveRecord as BaseActiveRecord;
 
-class ActiveRecord extends MongoActiveRecord
+class ActiveRecord extends BaseActiveRecord
 {
-	private $_subdocuments = [];
-	
-	public function init()
-	{
-		parent::init();
-		foreach($this->subdocuments() as $k => $options){
-			if(is_numeric($k)){
-				$this->_subdocuments[$options] = [];
-			}else{
-				$this->_subdocuments[$k] = $options;
-			}
-		}
-	}
-
     private $_formName;
 
     public function formName()
@@ -39,68 +23,4 @@ class ActiveRecord extends MongoActiveRecord
     {
         $this->_formName = $formName;
     }
-	
-	public static function find()
-	{
-		return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
-	}
-	
-	public function subdocuments()
-	{
-		return [];
-	}
-	
-	public function getDirtyAttributes($names = null)
-	{
-		$attributes = parent::getDirtyAttributes($names);
-		return $this->getRawAttributes($attributes);
-	}
-	
-	public function getAttributes($names = null, $except = [])
-	{
-		$attributes = parent::getAttributes($names, $except);
-		return $this->getRawAttributes($attributes);
-	}
-	
-	public function getRawAttributes($attributes)
-	{
-		foreach($attributes as $k => $v){
-			if(is_array($v)){
-				$attributes[$k] = $this->getRawAttributes($v);
-			}elseif($v instanceof Subdocument){
-				$attributes[$k] = $v->getRawAttributes();
-			}
-		}
-		return $attributes;
-	}
-	
-	public function createSubdocument($options, $value)
-	{
-		$options['value'] = $value;
-		return new Subdocument($options);
-	}
-	
-	public function hasSubdocument($name)
-	{
-		if(isset($this->_subdocuments[$name])){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	
-	public static function populateRecord($model, $row)
-	{
-		$columns = array_flip($model->attributes());
-		foreach ($row as $name => $value) {
-			if($model->hasSubdocument($name)){
-				$model->setAttribute($name, new Subdocument(['attribute' => $name, 'value' => $value]));
-			}elseif (isset($columns[$name])) {
-				$model->setAttribute($name, $value);
-			} elseif ($model->canSetProperty($name)) {
-				$model->$name = $value;
-			}
-		}
-		$model->setOldAttributes($model->getAttributes());
-	}
 }
